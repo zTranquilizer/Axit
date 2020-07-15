835e67774a0a8ddcec7961a7b321ff2a09b053f6
zTranquilizer <61971694+zTranquilizer@users.noreply.github.com>
2020-07-15 17:30:46 +0300

    upload files to server
    Testing!!!!!!

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src =
+		"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector("body").classList.add("webp");
+	} else {
+		document.querySelector("body").classList.add("no-webp");
+	}
+});
+
+$(document).ready(function () {
+	$(".header__burger").click(function (event) {
+		$(".header__burger, .header__menu").toggleClass("active-menu");
+		$("body").toggleClass('lock');
+	})
+	$(".tabs-menu__link").click(function (e) {
+		e.preventDefault();
+		$(".tabs-page__content").removeClass("active-tab");
+		$(".tabs-menu__link").removeClass("active-tab-bg");
+		//!Нужно доработать и разобраться что не так
+		// let tab_href = $(this).attr("href");
+		// let tab_data = $(this).attr("data-tab");
+		// if (tab_href && tab_data) {
+		// 	$(tab_href).addClass("active-tab");
+		// 	$(tab_data).addClass("active-tab-bg");
+		// }
+		//!НЕ ЗАБЫТЬ!!!!!!!
+		$(this).addClass("active-tab-bg");
+		$($(this).attr("href")).addClass('active-tab');
+	})
+	$(".tabs-menu__link:first").click();
+});
