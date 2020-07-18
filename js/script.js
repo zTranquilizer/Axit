3a7d6077c2651e98a91e158204fc2077d1077597
zTranquilizer <61971694+zTranquilizer@users.noreply.github.com>
2020-07-18 11:10:39 +0300

    upload correct files
    upload correct files

--- js/script.js
+++ js/script.js
@@ -1,59 +1,51 @@
 function testWebP(callback) {
-  var webP = new Image();
-  webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-  };
-  webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src =
+		"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
-  if (support == true) {
-    document.querySelector("body").classList.add("webp");
-  } else {
-    document.querySelector("body").classList.add("no-webp");
-  }
+	if (support == true) {
+		document.querySelector("body").classList.add("webp");
+	} else {
+		document.querySelector("body").classList.add("no-webp");
+	}
 });
 
 $(document).ready(function () {
-  $(".header__burger").click(function (event) {
-    $(".header__burger, .menu-header, .header").toggleClass("active-menu");
-    $("body").toggleClass("lock");
-  });
-  $(".full-main__img-scroll").click(function () {
-    elementClick = $(this).attr("href");
-    destination = $(elementClick).offset().top;
-    $("body,html").animate({ scrollTop: destination }, 1500);
-  });
-  $(".but-up").click(function () {
-    $("body,html").animate({ scrollTop: 0 }, 1000);
-  });
-});
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 115) {
-    $(".header").addClass("fixed-header");
-    $(".full-main__body").addClass("margin");
-  } else {
-    $(".header").removeClass("fixed-header");
-    $(".full-main__body").removeClass("margin");
-  }
-  if ($(window).width() > 767) {
-    if ($(this).scrollTop() > 400) {
-      $(".row-agency__item").slideDown(400, "linear");
-    } else {
-      $(".row-agency__item").slideUp(600, "linear");
-    }
-    if ($(this).scrollTop() != 0) {
-      $(".but-up").fadeIn(300, "linear");
-    } else {
-      $(".but-up").fadeOut(300, "linear");
-    }
-  }
+	$(".header__burger").click(function (event) {
+		$(".header__burger, .header__menu").toggleClass("active-menu");
+		$("body").toggleClass('lock');
+	})
+	$(".tabs-menu__link").click(function (e) {
+		e.preventDefault();
+		$(".tabs-page__content").removeClass("active-tab");
+		$(".tabs-menu__link").removeClass("active-tab-bg");
+		//!Нужно доработать и разобраться что не так
+		// let tab_href = $(this).attr("href");
+		// let tab_data = $(this).attr("data-tab");
+		// if (tab_href && tab_data) {
+		// 	$(tab_href).addClass("active-tab");
+		// 	$(tab_data).addClass("active-tab-bg");
+		// }
+		//!НЕ ЗАБЫТЬ!!!!!!!
+		$(this).addClass("active-tab-bg");
+		$($(this).attr("href")).addClass('active-tab');
+	})
+	$(".tabs-menu__link:first").click();
 
-  if ($(this).scrollTop() > 0) {
-    $(".but-up").addClass("unhidden");
-  }
-});
-$(window).on("load", function () {
-  $(".wrapper").addClass("loaded");
+	let progress = document.querySelector('.progress');
+	console.log(progress);
+	window.addEventListener("scroll", progressBar);
+	function progressBar(e) {
+		let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+		let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+		let per = (windowScroll / windowHeight) * 100;
+		console.log(Math.round(per));
+		progress.style.width = per + "%";
+	}
+
 });
